86ec8df7e9a5db7d72c849e54f1113fdb65e8c0e
lvfahim <mdfahim26320@gmail.com>
2025-12-13 18:38:39 +0600

    my webside degine is parfect

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -6,18 +6,24 @@ import Contact from './PageComponet/Contact';
 
 const Home = () => {
     return (
-        <div className='mt-10'>
-            <div className="mt-10">
-                <Hero></Hero>
+        <div className='mt-10 bg-gray-50'>
+
+            <div className="pb-10">
+                <Hero />
             </div>
-            <div className="mt-10">
-                <MostOldClub></MostOldClub>
-            </div>
-            <div className="mt-10">
-                <Contact></Contact>
-            </div>
-            <div className="">
-                <WhoItWork></WhoItWork>
+
+            <div className='w-10/12 mx-auto'>
+                <div className="py-10">
+                    <WhoItWork />
+                </div>
+
+                <div className="py-10">
+                    <MostOldClub />
+                </div>
+
+                <div className="pt-10">
+                    <Contact />
+                </div>
             </div>
         </div>
     );
